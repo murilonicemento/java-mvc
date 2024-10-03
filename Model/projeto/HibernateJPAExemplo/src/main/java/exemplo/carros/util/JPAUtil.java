31989b3069c1b3233a9c4be47010aade48c3720c
murilonicemento <murilo_nascimento22@outlook.com>
2024-10-03 20:03:44 -0300

    first commit

---
+++ Model/projeto/HibernateJPAExemplo/src/main/java/exemplo/carros/util/JPAUtil.java
@@ -0,0 +1,16 @@
+package exemplo.carros.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+	
+	private static final EntityManagerFactory FACTORY = Persistence
+			.createEntityManagerFactory("carros-pu");
+
+	public static EntityManager getEntityManager() { 
+		return FACTORY.createEntityManager();
+	}
+	
+}
